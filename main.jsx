cfb6c62929e149d978e014136bad6fc53366ed14
Daniel Snitkovskiy <Daniel@pc4d4uw.local>
2016-11-20 19:10:29 -0800

    Merge

--- main.jsx
+++ main.jsx
@@ -46,6 +46,27 @@ var Stat = React.createClass({
   }
 });
 
+var SortButtons = React.createClass({
+  render:function(){
+    return(
+      <div>
+        <a id={this.props.id + '-0'} onClick={this.props.clickEvent}>
+          <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
+              <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+        </a>
+        <a id={this.props.id + '-1'} onClick={this.props.clickEvent}>
+          <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"/>
+              <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+        </a>
+      </div>
+    )
+  }
+});
+
 /*This is a React component for the StatTable, which holds the headings
 As well as the rest of the individual "Stat" entries.
 Within the <tbody>, each entry in the'data' property that is passed to this component
@@ -56,16 +77,65 @@ Note: (d,i) => is equivalent to .map(function(d,i){}). [In case you haven't gott
 
 Styling courtesy of materialize.css*/
 var StatTable = React.createClass({
+  getInitialState:function(){
+    return ({sortCriteria:'published', order:1});
+  },
+  setSort:function(event){
+    var results = event.currentTarget.id.split('-');
+    var sortCriteria = results[0];
+    var order = results[1];
+    this.setState({sortCriteria:sortCriteria, order:order});
+
+  },
+  sortRows:function(order){
+    var sortCriteria = this.state.sortCriteria;
+    var order = this.state.order;
+    console.log(sortCriteria);
+    var sorted_Rows = this.props.data.sort(function(a,b){
+      a = a[sortCriteria].trim().toLowerCase();
+      b = b[sortCriteria].trim().toLowerCase();
+      if(sortCriteria == 'published'){
+        a = new Date(a);
+        b = new Date(b);
+      }
+      if(order == 0){
+        if(a < b){
+          return -1
+        }
+        else{
+          if(a > b){
+            return 1;
+          }
+        return 0;
+        }
+      }
+      else{
+        if(a < b){
+          return 1
+        }
+        else{
+          if(a > b){
+            return -1;
+          }
+        return 0;
+        }
+      }
+    });
+    return sorted_Rows;
+  },
   render:function(){
+    if(this.props.data){
+      this.sortRows();
+    }
     return(
       <div>
         <table className='striped'>
           <thead>
             <tr>
-                <th className='center-align' data-field="title">Title</th>
-                <th className='center-align' data-field="stat">Stat</th>
-                <th data-field="org">Organization</th>
-                <th data-field="published">Date Published</th>
+                <th className='center-align' data-field="title">Title<SortButtons id='title' clickEvent={this.setSort}/></th>
+                <th className='center-align' data-field="stat">Stat<SortButtons id='stat' clickEvent={this.setSort}/></th>
+                <th className='center-align' data-field="org">Organization<SortButtons id='org' clickEvent={this.setSort}/></th>
+                <th className='center-align' data-field="published">Date Published<SortButtons id='published' clickEvent={this.setSort}/></th>
             </tr>
           </thead>
           <tbody>
@@ -83,6 +153,138 @@ var StatSearch = React.createClass({
   getInitialState:function(){
     return ({title:'', org:'', stat:'', beginDate:'', endDate:''});
   },
+  componentDidMount:function(){
+    /**
+     * This is API key from the Google Developer Console
+     */
+    var CLIENT_ID = null;
+
+    /**
+     * This is the spreadsheet's ID from its URL
+     */
+    var SPREADSHEET_ID = null;
+
+    /**
+     * SCOPES determine an app's permissions when making API calls involving user data
+     */
+    var SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+
+    /**
+     * This is the set of cells that we want returned. E.g. A1:B2 would refer to these cells:
+     * | A1 | B1 |
+     * | A2 | B2 |
+     *
+     * TODO: figure out how to return all non-empty rows instead of hardcoding a large number of rows.
+     */
+    var RANGE = 'A2:D1000';
+
+    /**
+     * For reading a JSON configuration file
+     */
+    function readConfig(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        }
+        rawFile.send(null);
+    }
+
+    /**
+     * Set CLIENT_ID and SPREADSHEET_ID based on config file
+     */
+    function setIDsFromConfig(text) {
+      var data = JSON.parse(text);
+      CLIENT_ID = data.client_id;
+      SPREADSHEET_ID = data.sheet_id;
+    }
+
+    readConfig("config.json", setIDsFromConfig);
+
+    /**
+     * Check if current user has authorized this application
+     */
+    function checkAuth() {
+      gapi.auth.authorize(
+        {
+          'client_id': CLIENT_ID,
+          'scope': SCOPES.join(' '),
+          'immediate': true
+        }, handleAuthResult);
+    }
+
+    /**
+     * Handle response from authorization server.
+     *
+     * @param {Object} authResult Authorization result.
+     */
+    function handleAuthResult(authResult) {
+      var authorizeDiv = document.getElementById('authorize-div');
+      if (authResult && !authResult.error) {
+        // Hide auth UI, then load client library.
+        authorizeDiv.style.display = 'none';
+        loadSheetsApi();
+      } else {
+        // Show auth UI, allowing the user to initiate authorization by
+        // clicking authorize button.
+        authorizeDiv.style.display = 'inline';
+      }
+    }
+
+    /**
+     * Initiate auth flow in response to user clicking authorize button.
+     *
+     * @param {Event} event Button click event.
+     */
+    function handleAuthClick(event) {
+      gapi.auth.authorize(
+        {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+        handleAuthResult);
+      return false;
+    }
+
+    var test_data = [];
+
+    /**
+     * Load Sheets API client library
+     */
+    function loadSheetsApi() {
+      var discoveryUrl =
+          'https://sheets.googleapis.com/$discovery/rest?version=v4';
+      gapi.client.load(discoveryUrl).then(processSheetsData);
+    }
+
+    /**
+     * Stuff rows of stats from the spreadsheet into a JSON object
+     */
+    function processSheetsData() {
+      gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: SPREADSHEET_ID,
+        range: RANGE
+      }).then(function(response) {
+        var range = response.result;
+        if (range.values.length > 0) {
+          // This part turns the array of arrays returned by the API into a JSON resembling the hardcoded 'test_data' object we had before.
+          for (i = 0; i < range.values.length; i++) {
+            var row = range.values[i];
+            test_data[i] = {
+              'title'     : row[0],
+              'stat'      : row[1],
+              'org'       : row[2],
+              'pub_date'  : row[3]
+            }
+          }
+        } else {
+          console.log('No data found within the specified range.');
+        }
+      }, function(response) {
+        console.log('Error. Sheets API response: ' + response.result.error.message);
+      });
+    }
+  },
   // Sets the searchTerm and searchCriteria to the event's value and id, respectively.
   // This determines what will be searched and what that search will be on.
   filter:function(event){
@@ -96,22 +298,16 @@ var StatSearch = React.createClass({
 
       for (var searchCriteria in this.state)
       {
-        console.log(searchCriteria);
         var searchTerm = this.state[searchCriteria];
-        console.log(searchTerm);
         if (searchTerm.length > 0)
         {
           if (searchCriteria == 'beginDate')
           {
             var beginElements = searchTerm.split("-");
             var beginDate = new Date(beginElements[0], beginElements[1], beginElements[2]);
-            console.log("begin Date:");
-            console.log(beginDate);
             stats = stats.filter(function(stat) {
               var statElements = stat['published'].split("/");
               var date = new Date(statElements[2], statElements[0], statElements[1]);
-              console.log("Date:");
-              console.log(date);
               if (date > beginDate)
                 return stat;
               else
@@ -122,13 +318,9 @@ var StatSearch = React.createClass({
           {
             var endElements = searchTerm.split("-");
             var endDate = new Date(endElements[0], endElements[1], endElements[2]);
-            console.log("End Date:");
-            console.log(endDate);
             stats = stats.filter(function(stat) {
               var statElements = stat['published'].split("/");
               var date = new Date(statElements[2], statElements[0], statElements[1]);
-              console.log("Date:");
-              console.log(date);
               if (date < endDate)
                 return stat;
               else
@@ -141,8 +333,10 @@ var StatSearch = React.createClass({
             stats = stats.filter(function(stat){
               if (stat[searchCriteria].toLowerCase().includes(searchTerm.toLowerCase()))
                 return stat;
-              else
+              }
+              else{
                 return null;
+              }
             });
           }
         }
@@ -158,15 +352,15 @@ var StatSearch = React.createClass({
               <input placeholder="Enter an Organization" id="org" type="text" className="validate" onChange={this.filter}></input>
               <label>Org Search</label>
             </div>
-            <div className="input-field col s6">
+            <div className="input-field col s12">
               <input placeholder="Enter a Stat" id="stat" type="text" className="validate" onChange={this.filter}></input>
               <label>Stat Search</label>
             </div>
-            <div className="input-field col s3">
-              <input id="beginDate" type="date" onChange={this.filter}></input>
+            <div className="input-field col s6">
+              <input placeholder='begin date' id="beginDate" type="date" onChange={this.filter}></input>
             </div>
-            <div className="input-field col s3">
-              <input id="endDate" type="date" onChange={this.filter}></input>
+            <div className="input-field col s6">
+              <input placeholder='endDate' id="endDate" type="date" onChange={this.filter}></input>
             </div>
             <div className='col s12'>
               <StatTable data={stats}/>
@@ -269,11 +463,64 @@ var AddStat = React.createClass({
   }
 });
 
+
+var test_data1 = [
+  {
+  'title':'Cost of Cyber Crime',
+  'org':'Ponemon Institute',
+  'published': '10/09/2016',
+  'stat': 'Average annual losses to companies worldwide now exceed $7.7 million, with studied companies losing up to $65 million.',
+  },
+  {
+  'title':'Cost of Cyber Crime',
+  'org':'Ponemon Institute',
+  'published': '11/09/2016',
+  'stat': 'Average annual losses to companies worldwide now exceed $7.7 million, with studied companies losing up to $65 million.',
+  },
+  {
+  'title':'How Much for this Breach?',
+  'org':'Ponemon Institute',
+  'published': '10/09/2016',
+  'stat': '$7.7 million, with studied companies losing up to $65 million.',
+  },
+  {
+  'title':'Can you believe it\'s not butter?',
+  'org':'Ponemon Institute',
+  'published': '10/09/2016',
+  'stat': 'Studied companies losing up to $65 million.',
+  },
+  {
+  'title':'Can you believe it\'s not butter?',
+  'org':'Ponemon Institute',
+  'published': '9/09/2016',
+  'stat': 'Studied companies losing up to $65 million.',
+  },
+  {
+  'title':'Can you believe it\'s not butter?',
+  'org':'Ponemon Institute',
+  'published': '10/09/2015',
+  'stat': 'Studied companies losing up to $65 million.',
+  },
+  {
+  'title':'Can you believe it\'s not butter?',
+  'org':'Institute',
+  'published': '10/09/2015',
+  'stat': 'Studied companies losing up to $65 million.',
+  },
+  {
+  'title':'Can you believe it\'s not butter?',
+  'org':'Verizon',
+  'published': '10/09/2015',
+  'stat': 'Studied companies losing up to $65 million.',
+  },
+];
+
+
 // The ReactDOM.render renders components to the dom. It takes 2 args:
 // 1. Component(s) to be rendered and 2. Location to render specified component(s)
 ReactDOM.render(
   <div>
-    <StatSearch data={test_data} />
+    <StatSearch data={test_data1} />
     <AddStat />
   </div>,
     document.querySelector('#root')
